a5d88330072936945297a75d188dc60063c166e4
FelipeVConde <felipe.conde@sptech.school.com>
2023-11-15 14:22:02 -0300

    reorganização

--- Site/app.js
+++ Site/app.js
@@ -1,5 +1,5 @@
 process.env.AMBIENTE_PROCESSO = "desenvolvimento";
-
+// process.env.AMBIENTE_PROCESSO = "producao";
 
 var express = require("express");
 var cors = require("cors");
@@ -8,7 +8,7 @@ var PORTA = process.env.AMBIENTE_PROCESSO == "desenvolvimento" ? 3333 : 8080;
 
 var app = express();
 
-
+var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 
 
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
+app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 
 
