942dd1e65f7cfeb933cf7ae262cb5f6a0ef3f3d2
Felipe Conde <felipe.conde@sptech.school>
2023-11-23 20:14:57 -0300

    colocação de personagens

--- Site/public/JS/personagens.js
+++ Site/public/JS/personagens.js
@@ -161,19 +161,27 @@ ListaIcon = [
 ListaDescricao = [
   'O misterioso Albedo é o Chefe Alquimista e Capitão da Equipe de Investigação dos Cavaleiros de Favonius, com Sucrose como sua assistente. Ele tem um desejo infinito de aprender sobre o mundo de Teyvat, estudando cuidadosamente todos os objetos ao seu redor.',
   
-  ''
+  'O atual Escriba da Academia de Sumeru, um homem de grande inteligência e talento. Vive sua vida livre e despreocupado, sendo uma pessoa muito difícil de se encontrar.',
+
+
 ]
 ListaSkill1 =  [
-  'Ataque Normal<br> Realiza até 5 golpes rápidos.<br><br> Ataque Carregado<br>  Consome uma certa quantidade de Stamina para desferir 2 golpes rápidos com a espada.<br><br> Ataque Imersivo<br> Mergulha em pleno ar para golpear o chão, causando dano aos inimigos ao longo do caminho e infligindo Dano em uma AdE durante o impacto.'
+  'Ataque Normal<br> Realiza até 5 golpes rápidos.<br><br> Ataque Carregado<br>  Consome uma certa quantidade de Stamina para desferir 2 golpes rápidos com a espada.<br><br> Ataque Imersivo<br> Mergulha em pleno ar para golpear o chão, causando dano aos inimigos ao longo do caminho e infligindo Dano em uma AdE durante o impacto.',
+
+  'Ataque Normal<br> Realiza até 5 golpes rápidos.<br><br>Ataque Carregado<br> Consome uma certa quantidade de Stamina para desferir 2 golpes rápidos com a espada.<br><br> Ataque Imersivo<br> Mergulha em pleno ar para golpear o chão, causando dano aos inimigos ao longo do caminho e infligindo Dano em uma AdE durante o impacto.',
+
 
 ]
 ListaSkill2 = [
-  'A criação da Radiação Solar causa Dano Geo em uma AdE<br><br> Radiação Solar  Tem as seguintes propriedades: Quando inimigos dentro da área da Radiação Solar são atingidos, a Radiação Solar gerará Flores Transitórias que causam Dano Geo em uma AdE. Dano causado escala conforme a DEF de Albedo.<br> Flores Transitórias só podem ser gerada uma vez a cada 2s. Quando um personagem está localizado no local da Radiação Solar, a Radiação Solar irá acumular o poder Geo, erguendo-os em um pódio cristalizado. Apenas um pódio cristalizado pode existir por vez.<br> Somente é possível existir uma Radiação Solar por vez feita por Albedo, e é considerada como uma Construção Geo.<br><br> Manter pressionado para designar a localização da habilidade.'
+  'A criação da Radiação Solar causa Dano Geo em uma AdE<br><br> Radiação Solar  Tem as seguintes propriedades: Quando inimigos dentro da área da Radiação Solar são atingidos, a Radiação Solar gerará Flores Transitórias que causam Dano Geo em uma AdE. Dano causado escala conforme a DEF de Albedo.<br> Flores Transitórias só podem ser gerada uma vez a cada 2s. Quando um personagem está localizado no local da Radiação Solar, a Radiação Solar irá acumular o poder Geo, erguendo-os em um pódio cristalizado. Apenas um pódio cristalizado pode existir por vez.<br> Somente é possível existir uma Radiação Solar por vez feita por Albedo, e é considerada como uma Construção Geo.<br><br> Manter pressionado para designar a localização da habilidade.',
+
+  'Faz uma investida rapidamente. Ao terminar essa investida rápida, causa Dano Dendro contra os oponentes ao seu redor e acumula "Espelho Cortante". Será lançada de maneira de diferente ao Manter Pressionado.<br><br> Manter Pressionado<br> Entra em um estado de mira para ajustar a direção em que vai avançar.<br><br> Espelho Cortante<br> Ao lançar, Alhaitham gerará 1 Espelho Cortante; se ainda não possuir nenhum Espelho Cortante, gerará um adicional. O Espelho Cortante possui os seguintes efeitos:<br>· Ao possuir Espelhos Cortantes, os Ataques Normais, Carregados e Imersivos de Alhaitham serão convertidos em Dano Dendro que não pode ser substituído por outros elementos;<br>· Quando o ataque descrito acima atinge um inimigo, o Espelho Cortante fará o ataque Cortina de Luz, causando Dano Dendro em AdE baseado na quantidade de Espelhos Cortantes;<br>· É possível possuir até 3 Espelhos Cortantes;<br>· Os Espelhos Cortantes desaparecerão com o passar do tempo, e todos serão eliminados quando Alhaitham sair do campo de batalha.',
 
 ]
 ListaSkill3 = [
-  'Sob o comando de Albedo, os cristais Geo surgem e explodem, causando Dano Geo em uma AdE na frente dele. Se houver Radiação Solar no campo criado pelo próprio Albedo, 7 Flores Fatais serão geradas na zona da Radiação Solar, florescendo violentamente e causando Dano Geo em uma AdE.<br><br> O Dano causado pela explosão e da Flor Fatal não gerará Flores Transitórias.'
+  'Sob o comando de Albedo, os cristais Geo surgem e explodem, causando Dano Geo em uma AdE na frente dele. Se houver Radiação Solar no campo criado pelo próprio Albedo, 7 Flores Fatais serão geradas na zona da Radiação Solar, florescendo violentamente e causando Dano Geo em uma AdE.<br><br> O Dano causado pela explosão e da Flor Fatal não gerará Flores Transitórias.',
 
+  'Cria um reino especial, causando várias instâncias de Dano Dendro em AdE.<br>  Se ao lançar possuir Espelhos Cortantes, consumirá todos eles para aumentar o número de vezes de Dano causado.<br> Depois de 2s, se ao lançar essa Habilidade tiver consumido 0/1/2/3 Espelho(s) Cortante(s), gerará 3/2/1/0 Espelho(s) Cortante(s) para Alhaitham.'
 ]
 
 
