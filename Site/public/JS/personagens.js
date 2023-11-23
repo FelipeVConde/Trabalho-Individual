753cff57700b1e3348cc5d93f0aa70db0329a412
Felipe Conde <felipe.conde@sptech.school>
2023-11-23 19:53:02 -0300

    variaveis personagens

--- Site/public/JS/personagens.js
+++ Site/public/JS/personagens.js
@@ -1,172 +1,241 @@
 ListaPersonagem = [
-    'albedo',
-    'alhaitham',
-    'aloy',
-    'amber',
-    'andarilho',
-    'ayaka',
-    'ayato',
-    'baizhu',
-    'barbara',
-    'beidou',
-    'bennett',
-    'candace',
-    'charlotte',
-    'chevreuse',
-    'chongyun',
-    'collei',
-    'cyno',
-    'dehya',
-    'diluc',
-    'diona',
-    'dori',
-    'eula',
-    'faruzan',
-    'fischl',
-    'freminet',
-    'furina',
-    'ganyu',
-    'gorou',
-    'heizou',
-    'hu tao',
-    'itto',
-    'jean',
-    'kaeya',
-    'kaveh',
-    'kazuha',
-    'keqing',
-    'kirara',
-    'klee',
-    'kokomi',
-    'kuki',
-    'layla',
-    'lisa',
-    'lumine',
-    'lynnette',
-    'lyney',
-    'mika',
-    'mona',
-    'nahida',
-    'navia',
-    'neuvillette',
-    'nilou',
-    'ningguang',
-    'noelle',
-    'qiqi',
-    'raiden',
-    'razor',
-    'rosaria',
-    'sara',
-    'sayu',
-    'shenhe',
-    'sucrose',
-    'tartaglia',
-    'thoma',
-    'tighnari',
-    'traveler',
-    'venti',
-    'wriothesley',
-    'xiangling',
-    'xiao',
-    'xingqiu',
-    'xinyan',
-    'yae',
-    'yanfei',
-    'yaoyao',
-    'yelan',
-    'yoimiya',
-    'yun jin',
-    'zhongli'
-  ]
+  'albedo',
+  'alhaitham',
+  'aloy',
+  'amber',
+  'andarilho',
+  'ayaka',
+  'ayato',
+  'baizhu',
+  'barbara',
+  'beidou',
+  'bennett',
+  'candace',
+  'charlotte',
+  'chevreuse',
+  'chongyun',
+  'collei',
+  'cyno',
+  'dehya',
+  'diluc',
+  'diona',
+  'dori',
+  'eula',
+  'faruzan',
+  'fischl',
+  'freminet',
+  'furina',
+  'ganyu',
+  'gorou',
+  'heizou',
+  'hu tao',
+  'itto',
+  'jean',
+  'kaeya',
+  'kaveh',
+  'kazuha',
+  'keqing',
+  'kirara',
+  'klee',
+  'kokomi',
+  'kuki',
+  'layla',
+  'lisa',
+  'lumine',
+  'lynette',
+  'lyney',
+  'mika',
+  'mona',
+  'nahida',
+  'navia',
+  'neuvillette',
+  'nilou',
+  'ningguang',
+  'noelle',
+  'qiqi',
+  'raiden',
+  'razor',
+  'rosaria',
+  'sara',
+  'sayu',
+  'shenhe',
+  'sucrose',
+  'tartaglia',
+  'thoma',
+  'tighnari',
+  'traveler',
+  'venti',
+  'wriothesley',
+  'xiangling',
+  'xiao',
+  'xingqiu',
+  'xinyan',
+  'yae',
+  'yanfei',
+  'yaoyao',
+  'yelan',
+  'yoimiya',
+  'yun jin',
+  'zhongli'
+]
 ListaIcon = [
-        "assets/imgs/iconCaracter/Albedo.webp",
-        "assets/imgs/iconCaracter/Alhaitham.webp",
-        "assets/imgs/iconCaracter/Aloy.webp",
-        "assets/imgs/iconCaracter/Amber.webp",
-        "assets/imgs/iconCaracter/Andarilho.webp",
-        "assets/imgs/iconCaracter/ayaka.webp",
-        "assets/imgs/iconCaracter/Ayato.webp",
-        "assets/imgs/iconCaracter/Baizhu.webp",
-        "assets/imgs/iconCaracter/Barbara.webp",
-        "assets/imgs/iconCaracter/Beidou.webp",
-        "assets/imgs/iconCaracter/Bennett.webp",
-        "assets/imgs/iconCaracter/Candace.webp",
-        "assets/imgs/iconCaracter/Charlotte.webp",
-        "assets/imgs/iconCaracter/Chevreuse.webp",
-        "assets/imgs/iconCaracter/Chongyun.webp",
-        "assets/imgs/iconCaracter/Collei.webp",
-        "assets/imgs/iconCaracter/Cyno.webp",
-        "assets/imgs/iconCaracter/Dehya.webp",
-        "assets/imgs/iconCaracter/Diluc.webp",
-        "assets/imgs/iconCaracter/Diona.webp",
-        "assets/imgs/iconCaracter/Dori.webp",
-        "assets/imgs/iconCaracter/Eula.webp",
-        "assets/imgs/iconCaracter/Faruzan.webp",
-        "assets/imgs/iconCaracter/Fischl.webp",
-        "assets/imgs/iconCaracter/Freminet.webp",
-        "assets/imgs/iconCaracter/Furina.webp",
-        "assets/imgs/iconCaracter/Ganyu.webp",
-        "assets/imgs/iconCaracter/Gorou.webp",
-        "assets/imgs/iconCaracter/Heizou.webp",
-        "assets/imgs/iconCaracter/Hutao.webp",
-        "assets/imgs/iconCaracter/Itto.webp",
-        "assets/imgs/iconCaracter/Jean.webp",
-        "assets/imgs/iconCaracter/Kaeya.webp",
-        "assets/imgs/iconCaracter/Kaveh.webp",
-        "assets/imgs/iconCaracter/Kazuha.webp",
-        "assets/imgs/iconCaracter/Keqing.webp",
-        "assets/imgs/iconCaracter/Kirara.webp",
-        "assets/imgs/iconCaracter/Klee.webp",
-        "assets/imgs/iconCaracter/Kokomi.webp",
-        "assets/imgs/iconCaracter/Kuki.webp",
-        "assets/imgs/iconCaracter/Layla.webp",
-        "assets/imgs/iconCaracter/Lisa.webp",
-        "assets/imgs/iconCaracter/Lumine.webp",
-        "assets/imgs/iconCaracter/Lynette.webp",
-        "assets/imgs/iconCaracter/Lyney.webp",
-        "assets/imgs/iconCaracter/Mika.webp",
-        "assets/imgs/iconCaracter/Mona.webp",
-        "assets/imgs/iconCaracter/Nahida.webp",
-        "assets/imgs/iconCaracter/Navia.webp",
-        "assets/imgs/iconCaracter/Neuvillette.webp",
-        "assets/imgs/iconCaracter/Nilou.webp",
-        "assets/imgs/iconCaracter/Ningguang.webp",
-        "assets/imgs/iconCaracter/Noelle.webp",
-        "assets/imgs/iconCaracter/Qiqi.webp",
-        "assets/imgs/iconCaracter/Raiden.webp",
-        "assets/imgs/iconCaracter/Razor.webp",
-        "assets/imgs/iconCaracter/Rosaria.webp",
-        "assets/imgs/iconCaracter/Sara.webp",
-        "assets/imgs/iconCaracter/Sayu.webp",
-        "assets/imgs/iconCaracter/Shenhe.webp",
-        "assets/imgs/iconCaracter/Sucrose.webp",
-        "assets/imgs/iconCaracter/Tartaglia.webp",
-        "assets/imgs/iconCaracter/Thoma.webp",
-        "assets/imgs/iconCaracter/Tighnari.webp",
-        "assets/imgs/iconCaracter/Traveler.webp",
-        "assets/imgs/iconCaracter/Venti.webp",
-        "assets/imgs/iconCaracter/Wriothesley.webp",
-        "assets/imgs/iconCaracter/Xiangling.webp",
-        "assets/imgs/iconCaracter/Xiao.webp",
-        "assets/imgs/iconCaracter/Xingqiu.webp",
-        "assets/imgs/iconCaracter/Xinyan.webp",
-        "assets/imgs/iconCaracter/Yae.webp",
-        "assets/imgs/iconCaracter/Yanfei.webp",
-        "assets/imgs/iconCaracter/Yaoyao.webp",
-        "assets/imgs/iconCaracter/Yelan.webp",
-        "assets/imgs/iconCaracter/Yoimiya.webp",
-        "assets/imgs/iconCaracter/Yunjin.webp",
-        "assets/imgs/iconCaracter/Zhongli.webp",
-  ]
+  "assets/imgs/iconCaracter/Albedo.webp",
+  "assets/imgs/iconCaracter/Alhaitham.webp",
+  "assets/imgs/iconCaracter/Aloy.webp",
+  "assets/imgs/iconCaracter/Amber.webp",
+  "assets/imgs/iconCaracter/Andarilho.webp",
+  "assets/imgs/iconCaracter/ayaka.webp",
+  "assets/imgs/iconCaracter/Ayato.webp",
+  "assets/imgs/iconCaracter/Baizhu.webp",
+  "assets/imgs/iconCaracter/Barbara.webp",
+  "assets/imgs/iconCaracter/Beidou.webp",
+  "assets/imgs/iconCaracter/Bennett.webp",
+  "assets/imgs/iconCaracter/Candace.webp",
+  "assets/imgs/iconCaracter/Charlotte.webp",
+  "assets/imgs/iconCaracter/Chevreuse.webp",
+  "assets/imgs/iconCaracter/Chongyun.webp",
+  "assets/imgs/iconCaracter/Collei.webp",
+  "assets/imgs/iconCaracter/Cyno.webp",
+  "assets/imgs/iconCaracter/Dehya.webp",
+  "assets/imgs/iconCaracter/Diluc.webp",
+  "assets/imgs/iconCaracter/Diona.webp",
+  "assets/imgs/iconCaracter/Dori.webp",
+  "assets/imgs/iconCaracter/Eula.webp",
+  "assets/imgs/iconCaracter/Faruzan.webp",
+  "assets/imgs/iconCaracter/Fischl.webp",
+  "assets/imgs/iconCaracter/Freminet.webp",
+  "assets/imgs/iconCaracter/Furina.webp",
+  "assets/imgs/iconCaracter/Ganyu.webp",
+  "assets/imgs/iconCaracter/Gorou.webp",
+  "assets/imgs/iconCaracter/Heizou.webp",
+  "assets/imgs/iconCaracter/Hutao.webp",
+  "assets/imgs/iconCaracter/Itto.webp",
+  "assets/imgs/iconCaracter/Jean.webp",
+  "assets/imgs/iconCaracter/Kaeya.webp",
+  "assets/imgs/iconCaracter/Kaveh.webp",
+  "assets/imgs/iconCaracter/Kazuha.webp",
+  "assets/imgs/iconCaracter/Keqing.webp",
+  "assets/imgs/iconCaracter/Kirara.webp",
+  "assets/imgs/iconCaracter/Klee.webp",
+  "assets/imgs/iconCaracter/Kokomi.webp",
+  "assets/imgs/iconCaracter/Kuki.webp",
+  "assets/imgs/iconCaracter/Layla.webp",
+  "assets/imgs/iconCaracter/Lisa.webp",
+  "assets/imgs/iconCaracter/Lumine.webp",
+  "assets/imgs/iconCaracter/Lynette.webp",
+  "assets/imgs/iconCaracter/Lyney.webp",
+  "assets/imgs/iconCaracter/Mika.webp",
+  "assets/imgs/iconCaracter/Mona.webp",
+  "assets/imgs/iconCaracter/Nahida.webp",
+  "assets/imgs/iconCaracter/Navia.webp",
+  "assets/imgs/iconCaracter/Neuvillette.webp",
+  "assets/imgs/iconCaracter/Nilou.webp",
+  "assets/imgs/iconCaracter/Ningguang.webp",
+  "assets/imgs/iconCaracter/Noelle.webp",
+  "assets/imgs/iconCaracter/Qiqi.webp",
+  "assets/imgs/iconCaracter/Raiden.webp",
+  "assets/imgs/iconCaracter/Razor.webp",
+  "assets/imgs/iconCaracter/Rosaria.webp",
+  "assets/imgs/iconCaracter/Sara.webp",
+  "assets/imgs/iconCaracter/Sayu.webp",
+  "assets/imgs/iconCaracter/Shenhe.webp",
+  "assets/imgs/iconCaracter/Sucrose.webp",
+  "assets/imgs/iconCaracter/Tartaglia.webp",
+  "assets/imgs/iconCaracter/Thoma.webp",
+  "assets/imgs/iconCaracter/Tighnari.webp",
+  "assets/imgs/iconCaracter/Traveler.webp",
+  "assets/imgs/iconCaracter/Venti.webp",
+  "assets/imgs/iconCaracter/Wriothesley.webp",
+  "assets/imgs/iconCaracter/Xiangling.webp",
+  "assets/imgs/iconCaracter/Xiao.webp",
+  "assets/imgs/iconCaracter/Xingqiu.webp",
+  "assets/imgs/iconCaracter/Xinyan.webp",
+  "assets/imgs/iconCaracter/Yae.webp",
+  "assets/imgs/iconCaracter/Yanfei.webp",
+  "assets/imgs/iconCaracter/Yaoyao.webp",
+  "assets/imgs/iconCaracter/Yelan.webp",
+  "assets/imgs/iconCaracter/Yoimiya.webp",
+  "assets/imgs/iconCaracter/Yunjin.webp",
+  "assets/imgs/iconCaracter/Zhongli.webp",
+]
+ListaDescricao = [
+  'O misterioso Albedo é o Chefe Alquimista e Capitão da Equipe de Investigação dos Cavaleiros de Favonius, com Sucrose como sua assistente. Ele tem um desejo infinito de aprender sobre o mundo de Teyvat, estudando cuidadosamente todos os objetos ao seu redor.',
+  
+  ''
+]
+ListaSkill1 =  [
+  'Ataque Normal<br> Realiza até 5 golpes rápidos.<br><br> Ataque Carregado<br>  Consome uma certa quantidade de Stamina para desferir 2 golpes rápidos com a espada.<br><br> Ataque Imersivo<br> Mergulha em pleno ar para golpear o chão, causando dano aos inimigos ao longo do caminho e infligindo Dano em uma AdE durante o impacto.'
+
+]
+ListaSkill2 = [
+  'A criação da Radiação Solar causa Dano Geo em uma AdE<br><br> Radiação Solar  Tem as seguintes propriedades: Quando inimigos dentro da área da Radiação Solar são atingidos, a Radiação Solar gerará Flores Transitórias que causam Dano Geo em uma AdE. Dano causado escala conforme a DEF de Albedo.<br> Flores Transitórias só podem ser gerada uma vez a cada 2s. Quando um personagem está localizado no local da Radiação Solar, a Radiação Solar irá acumular o poder Geo, erguendo-os em um pódio cristalizado. Apenas um pódio cristalizado pode existir por vez.<br> Somente é possível existir uma Radiação Solar por vez feita por Albedo, e é considerada como uma Construção Geo.<br><br> Manter pressionado para designar a localização da habilidade.'
+
+]
+ListaSkill3 = [
+  'Sob o comando de Albedo, os cristais Geo surgem e explodem, causando Dano Geo em uma AdE na frente dele. Se houver Radiação Solar no campo criado pelo próprio Albedo, 7 Flores Fatais serão geradas na zona da Radiação Solar, florescendo violentamente e causando Dano Geo em uma AdE.<br><br> O Dano causado pela explosão e da Flor Fatal não gerará Flores Transitórias.'
+
+]
 
 
-function Buscar(){
-    for(posicao = 0; posicao <= ListaPersonagem.length; posicao += 1){
-      if(select_personagem.value == ListaPersonagem[posicao]){
-            div_informações.innerHTML =`
-            correto <img src="${ListaIcon[posicao]}" alt="${ListaPersonagem[posicao]}">
+function Buscar() {
+  for (posicao = 0; posicao <= ListaPersonagem.length; posicao += 1) {
+    if (select_personagem.value == ListaPersonagem[posicao]) {
+      div_informações.innerHTML = `
+      <div class="container1">
+        <div class="div_icon">
+          <img src="assets/imgs/iconCaracter/${ListaPersonagem[posicao]}.webp" alt="${ListaPersonagem[posicao]}">
+        </div>
+        <div class="div_contexto">
+          <h1>${ListaPersonagem[posicao]}</h1>
+          <p>
+            ${ListaDescricao[posicao]}
+          </p><br>
+          <img class="img_arma" src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_weapon.webp" alt="">
+          <br><br>
+          <img class="img_splash" src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_gacha_splash.webp" alt="">
+        </div>
+        <div class="div_card">
+          <img src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_gacha_card.webp" alt="${ListaPersonagem[posicao]}_gacha_card">
+        </div>
+      </div>
+
+      <div class="container3">
+        <h1>Habilidades</h1>
+        <div class="div_skill3">
+          <video autoplay muted loop src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_skill3.mp4"></video>
+
+          <p>
+            ${ListaSkill3[posicao]}
+          </p>
+        </div>
+        <div class="div_skill1">
+          <p>
+            ${ListaSkill1[posicao]}
+          </p>
+          <video autoplay muted loop src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_skill1.mp4"></video>
+        </div>
+        <div class="div_skill2">
+          <video autoplay muted loop src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_skill2.mp4"></video>
+          <p>
+            ${ListaSkill2[posicao]}
+          </p>
+        </div>
+      </div>
+
+      <div class="container2">
+        <h1>GALERIA</h1>
+        <div class="div_idle1">
+          <video autoplay muted loop src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_idle1.mp4"></video>
+        </div>
+        <div class="div_spin">
+          <video autoplay muted loop src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_360.mp4"></video>
+        </div>
+        <div class="div_idle2">
+          <video autoplay muted loop src="assets/Personagens/${ListaPersonagem[posicao]}/${ListaPersonagem[posicao]}_idle2.mp4"></video>
+        </div>
+      </div>
             `
-        }  
     }
-        
+  }
+
 }
