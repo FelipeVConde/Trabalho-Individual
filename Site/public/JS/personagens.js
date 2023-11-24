ecbf0c3a9e42391bbb16e7ce2dab6c34a695bc42
Felipe Conde <felipe.conde@sptech.school>
2023-11-24 00:16:12 -0300

    descrição personagens

--- Site/public/JS/personagens.js
+++ Site/public/JS/personagens.js
@@ -163,13 +163,107 @@ ListaDescricao = [
   
   'O atual Escriba da Academia de Sumeru, um homem de grande inteligência e talento. Vive sua vida livre e despreocupado, sendo uma pessoa muito difícil de se encontrar.',
 
+  'Protagonista do jogo Horizon Zero Dawn, e é apresentada como um personagem de Collab (colaboração) entre Guerrilla e miHoYo. Aloy foi lançada por tempo limitado na Versão 2.1 no PlayStation e na Versão 2.2 em todos as plataformas.',
 
+  'Única Aventureira restante dos Cavaleiros de Favonius. Ela está sempre pronta para ajudar os cidadãos de Mondstadt - seja em algo simples ou talvez uma tarefa mais desafiadora.',
+
+  'Um jovem andarilho de identidade misteriosa. Se veste como um shugenja, mas com certeza não age como um.',
+
+  'Filha mais velha do Clã Kamisato e irmã de Kamisato Ayato. Por ser educada, elegante e graciosa, as pessoas comuns não têm nada a dizer mal de Ayaka. Por causa de seu status social como a filha mais velha do Clã Kamisato da Comissão Yashiro e como Shirasagi Himegimi, Ayaka é vista como um modelo de perfeição.',
+
+  'O jovem promissor e chefe atual da Comissão Yashiro e do Clã Kamisato, é culto e educado, e sempre tem uma forma de fazer as coisas.',
+
+  'Dono do Chalé de Bubu. Ele anda sempre com sua cobra branca, Changsheng, pendurada no pescoço. Um grande conhecedor de medicina, suas verdadeiras intenções são um grande mistério.',
+
+  'Diaconisa da Igreja de Favonius e auto proclamada "ídolo" depois de aprender sobre eles com a intrépida aventureira Alice.',
+
+  'capitã da Frota Crux . Além de suas capacidades como capitã de frota e sua força imensa, muitos em Liyue a conhecem por sua falta de medo em relação ao Tianquan dos Qixing, Ningguang - uma característica que a outra mulher aprecia, mas fica irritada.',
+
+  'Um órfão descoberto por um aventureiro idoso quando bebê, Bennett foi criado na Guilda de Aventureiros de Mondstadt. Ele é o único membro da "Equipe de aventura de Benny", já que todos os outros deixaram a equipe depois de experimentar o infortúnio constante que o segue.',
+
+  'Descendente de Al-Ahmar e Guardiã da Vila Aaru, Candace é gentil com os viajantes que a visitam, mas é implacável com aqueles que violam as regras da vila.',
+
+  'trabalha como jornalista para Pássaro a Vapor, um jornal de Fontaine.',
+
+  'Chevreuse é um personagem Pyro anunciado em Genshin Impact.',
+
+  'Exorcista de Liyue , Chongyun nasceu com uma energia positiva excessiva, o que provou ser uma ajuda e um obstáculo. Isso o torna um exorcista muito eficaz, mas também significa que ele nunca viu um espírito para si mesmo - eles fogem antes mesmo que ele possa colocar os olhos neles. Também o tornou particularmente suscetível a qualquer tipo de calor ou emoção forte, o que leva a alguns ... resultados inesperados.',
+
+  'Uma Guarda-Florestal em Treinamento que patrulha a Floresta Avidya. Por trás de sua personalidade entusiasmada, encontra-se uma personalidade bastante introvertida.',
+
+  'Como General Mahamatra, Cyno lidera a Matra e infunde medo nos corações dos pesquisadores da Academia Sumeru.',
+
+  'Membro dos "Eremitas", uma organização mercenária que vaga pelos desertos de Sumeru. Ela é corajosa, poderosa e tem uma excelente reputação entre os colegas Eremitas.',
+
+  'Nascido na influente Família Ragnvindr, Diluc é o atual proprietário do Adega do Alvorecer e um nobre de alta estima na sociedade de Mondstadt. Embora um incidente anterior o tenha levado a se separar dos Cavaleiros de Favonius, ele continua a proteger Mondstadt à sua própria maneira.',
+
+  'Apesar de ser uma bartender em A Cauda do Gato, ela realmente despreza o álcool e não quer nada mais do que arruinar a indústria do vinho de Mondstadt - uma façanha mais fácil de dizer do que fazer já que seus clientes simplesmente amam suas bebidas.',
+
+  'A proprietária do Palácio de Alcazarzaray e uma comerciante de alta nível, cuja maior paixão é a Mora brilhante. "Lorde Sangemah Bay" é bem conhecida por sua capacidade de obter qualquer coisa que uma pessoa precise, desde que tenha Mora suficiente para pagar seus preços de cair o queixo.',
+
+  'Capitã de Reconhecimento dos Cavaleiros de Favonius, conhecida como Cavaleira das Ondas. Eula vem de uma linhagem aristocrática e é descendente do infame e tirânico Clã Lawrence que foi destituída pelos Cavaleiros de Favonius, ainda assim ela se junta aos Cavaleiros por motivos desconhecidos.',
+
+  'Ela é uma professora da Academia Sumeru e membro do Darshan Haravatat que desapareceu em algum momento no passado depois de ser exposta a resquícios do poder do Rei Escarlate, fazendo com que ela reviva o passado de Sumeru.',
+
+  'Investigadora da Guilda de Aventureiros de Mondstadt, acompanhado pelo corvo noturno Oz. Fischl afirma vir de um mundo além de Teyvat. Ela é uma aventureira ousada com teorias aparentemente bizarras que acabam se revelando verdadeiras.',
+
+  'Um jovem reservado muito habilidoso em mergulho. Sob seu comportamento distante e frio, reside um coração puro e isento de qualquer mácula.',
+
+  'Furina é a "humanidade" da Deusa da Justiça, Focalors, separada de sua divindade. Amaldiçoada com a imortalidade, ela supervisionou Fontaine como a Arconte Hydro atuante por 500 anos, a fim de realizar os planos do verdadeiro Arconte de enganar os Princípios Celestiais para salvar Fontaine de uma grande profecia. Após a morte dos Focalors e a destruição do Trono Divino do Arconte Hydro, ela abdicou de seu papel e agora vive sua vida como um ser humano comum.',
+
+  'Ela é uma emissária e a secretária dos Qixing de Liyue',
+
+  'Faz parte da Resistência de Sangonomiya contra o Decreto de Caça à Visão proclamado por Baal.',
+
+  'Um detetive de Inazuma[4] que trabalha para a Comissão Tenryou.',
+
+  'A 77ª Diretora da Funerária Wangsheng. Por algum motivo o nome da personagem em português é composto de uma única palavra, na maior partes das línguas ele é composto de duas palavras.',
+
+  'Conhecido por sua rivalidade unilateral com Kujou Sara depois que ela o derrotou e tomou sua Visão. Um descendente do oni carmesim, Itto também é o líder e fundador da Gangue Arataki.',
+
+  'Descendente do prestigioso Clã Gunnhildr, Jean é a Grande Mestre Intendente em exercício dos Cavaleiros de Favonius. Ela está sempre ocupada lidando com distúrbios em Mondstadt e, claro, trabalhando incansavelmente para manter a Cidade da Liberdade.',
+
+  'Capitão de Cavalaria dos Cavaleiros de Favonius. Ele é muito estimado pelo povo de Mondstadt - mesmo com todas as suas excentricidades e segredos.',
+
+  'Arquiteto renomado de Sumeru, é preocupado demais com muitas coisas. Um esteta perturbado pela realidade. Kaveh foi aluno do Darshan Kshahrewar da Academia de Sumeru e se formou com honras. Tendo acumulado muitas dívidas ele foi forçado a residir com Alhaitham, com quem briga constantemente.',
+
+  'samurai errante do outrora famoso Clã Kaedehara, Kazuha é um membro da tripulação da Frota Crux, acolhido por Beidou depois de ser declarado um criminoso por Baal por tirar uma visão de suas mãos.',
+
+  'Yuheng do Qixing de Liyue. Ela é alguém que procura suas próprias respostas ao invés de deixar o caos acontecer em Liyue. Keqing escolhe seu próprio caminho com seu próprio poder e habilidade, ao invés de deixar os Deuses escolherem seu caminho.',
+
+  'mensageira do Komaniya Express, empresa de entregas em Inazuma. Uma Nekomata que ama seu trabalho e a sociedade humana.',
+
+  'filha da intrépida — e destrutiva — aventureira Alice, Klee segue sua mãe de várias maneiras, para grande consternação dos Cavaleiros de Favonius a quem ela foi confiada.',
+
+  'Sacerdotisa Divina e é a principal autoridade da ilha de Watatsumi, na região de Inazuma. Além de trabalhar exaustivamente para garantir que sua população seja feliz, Kokomi também é muito bem versada na arte da guerra e pensa em centenas de possíveis desfechos para cada situação.',
+
+  'vice-líder da Gangue Arataki e responsável por consultas jurídicas, negociações comerciais, organização de banquetes e encomendação de vestuários.',
+
+  'Estudante do Rtawahist, especializada em astrologia teórica. Sofre de sonambulismo e insônia, ela está sempre enfrentando distúrbios de sono.',
+
+  'Outrora uma grande maga reconhecida como o graduada mais ilustre da Academia Sumeru em 200 anos, Lisa voltou a Mondstadt para se tornar a humilde Bibliotecária dos Cavaleiros de Favonius.',
+
+  'O Viajante é o protagonista jogável de Genshin Impact. No início do jogo existem 2 gêmeos, uma garota e um garoto, que viajam pelo universo até que são atacados por uma Deusa Desconhecido que captura um dos gêmeos, deixa o outro gêmeo desacordado por um tempo não determinado e sela seus caminhos de viagem deixando-os presos em Teyvat. Após um tempo o gêmeo desacordado acorda e parte em uma jornada em busca do irmão/irmã perdido, acompanhado pela Paimon.',
+
+  'Uma assistente de mágico de poucas palavras. Sua personalidade é tão indecifrável quanto a de um gato.',
+
+  'mágico astuto e habilidoso de Fontaine que sempre conquista o público com seus truques engenhosos e sua eloquência.',
+
+  'jovem cavaleiro nascido em uma família comum. Ele serve como Topógrafo da Linha de Frente na sua companhia. É tímido e cauteloso.',
+
+  'Astróloga de grande habilidade e igualmente grande orgulho, Mona fixou residência em Mondstadt para evitar incorrer na ira de sua mestra depois de ler inadvertidamente o diário dela.',
+
+  'corpo mortal da Lorde Menor Kusanali, atual Arconte Dendro.',
+
+  'Srta. Navia é, sem dúvida, uma senhorita perfeita de Fontaine.<br> Ela costumava vestir roupas complexas e usar um chapéu requintado, com um guarda-chuva decorado com laços e pedras preciosas em seu braço.<br> Então, ela corria pelas ruas e becos da Corte de Fontaine, pelos campos do Monte Automnequi e pelos recantos desconhecidos do Fleuve Cendre.',
+
+  ''
 ]
 ListaSkill1 =  [
   'Ataque Normal<br> Realiza até 5 golpes rápidos.<br><br> Ataque Carregado<br>  Consome uma certa quantidade de Stamina para desferir 2 golpes rápidos com a espada.<br><br> Ataque Imersivo<br> Mergulha em pleno ar para golpear o chão, causando dano aos inimigos ao longo do caminho e infligindo Dano em uma AdE durante o impacto.',
 
   'Ataque Normal<br> Realiza até 5 golpes rápidos.<br><br>Ataque Carregado<br> Consome uma certa quantidade de Stamina para desferir 2 golpes rápidos com a espada.<br><br> Ataque Imersivo<br> Mergulha em pleno ar para golpear o chão, causando dano aos inimigos ao longo do caminho e infligindo Dano em uma AdE durante o impacto.',
-
+  
 
 ]
 ListaSkill2 = [
